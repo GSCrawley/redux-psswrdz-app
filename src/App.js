dcf5179bab2c0b6be9f2612284c3ffbd8e61c963
GSCrawley <gscrawley@gmail.com>
2020-06-28 12:43:23 -0700

    password list created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
 import React, { Component } from 'react';
 import './App.css';
 import Password from './password';
 
+const store = createStore(reducers)
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Password />
-        
-      </div>
+        <Provider store={store}>
+        <div className="App">
+          <Password />
+        </div>
+      </Provider>
     );
   }
 }
